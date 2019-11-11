42ec9cc0dacd597964561b5f27b9a2124330da51
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-11-11 10:36:41 -0500

    Init Commit

--- Lab-DataGrid/MainWindow.xaml.cs
+++ Lab-DataGrid/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,37 @@ namespace Lab_DataGrid
     /// </summary>
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            string connString = @"server=(LocalDB)\MSSQLLocalDB;" +
+            "integrated security=SSPI;" +
+            "database=Employee";
+
+            SqlConnection sqlConn;
+            sqlConn = new SqlConnection(connString);
+
+            sqlConn.Open(); // Open the connection
+
+            // Setup the SQL command
+            string sql = "SELECT * FROM Employee";
+            SqlCommand command = new SqlCommand(sql, sqlConn);
+
+            // Retrieve the data from the database
+            SqlDataReader reader = command.ExecuteReader();
+
+            //*************************************************
+            // Associate the DataGrid with the SqlDataReader.
+            // This will automatically populate the DataGrid.
+            //*************************************************
+            dataGridEmployee1.ItemsSource = reader;
+            
+
         }
     }
 }
